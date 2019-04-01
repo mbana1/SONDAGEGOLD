bd9604c2fe025495dee7bddbff3d39acf913aef3
mbana1 <mbana1@yahoo.com>
2019-04-02 01:19:22 +0200

    02/04/2019

---
+++ SONDAGEGOLD/JavaScript/jquery.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#texteIntroduction").hide().show(1000)
+    $("BoutonMenu").hide().show(1000)
+   
+    $('input').val().empty()(function () { $('.mm').hide() });
+    if ($["name=checkbox3"].val() == "") {
+        $('.mm').hide();
+    }
+    else {
+        $('.mm').show();
+    }
+});
+
